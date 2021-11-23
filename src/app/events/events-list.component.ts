a80d5d4ba0af6a5433c1d956bda038b1d7a2cff0
Abdullahi95 <59885578+Abdullahi95@users.noreply.github.com>
2021-11-23 20:28:09 +0000

    Creating Your First Data-bound Component.

---
+++ src/app/events/events-list.component.ts
@@ -0,0 +1,24 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'events-lists',
+    templateUrl: './events-list.component.html'
+})
+
+export class EventListComponent{
+
+event = {
+    id: 1,
+    name: "Angular Connect",
+    date: "23/11/2021",
+    time: '10:00 am',
+    price: 599.9,
+    imageUrl: '/assets/images/angularconnect-shield.png',
+    location: {
+        address: "1057 DT",
+        city: 'London',
+        country: 'England'
+    }
+}
+
+}
