7a144b77f54737ae0478c3e6bf1614762451d8ab
Abdullahi95 <59885578+Abdullahi95@users.noreply.github.com>
2021-11-23 19:54:05 +0000

    Accessing Static Files

--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'events-app',
-  template: '<h1>Events app</h1>'
+  templateUrl: './events-app.component.html'
 })
 export class EventsAppComponent {
   title = 'ng-fundamentals';
