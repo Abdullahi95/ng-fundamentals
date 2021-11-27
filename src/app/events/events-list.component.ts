ed3fc22559de6fb6643ee0a72e7f3e3be9a64c6e
Abdullahi95 <59885578+Abdullahi95@users.noreply.github.com>
2021-11-27 15:21:33 +0000

    Repeating Data with *NgFor

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -1,23 +1,10 @@
 import { Component } from '@angular/core';
+import { events } from '../eventsdata';
 
 @Component({
   selector: 'events-lists',
   templateUrl: './events-list.component.html',
 })
 export class EventListComponent {
-  event1 = {
-    id: 1,
-    name: 'Angular Connect',
-    date: '23/11/2021',
-    time: '10:00 am',
-    price: 599.9,
-    imageUrl: '/assets/images/angularconnect-shield.png',
-    location: {
-      address: '1057 DT',
-      city: 'London',
-      country: 'England',
-    },
-  };
-
-  
+  eventData = events;
 }
